74f402c8d39422be690cc6a3882da61586b95932
Narasimha Sai Srinivas Machavaram <124352444+MNSSCHARI@users.noreply.github.com>
2025-06-08 17:21:41 +0530

    latest changes

--- tests/04_Code_Frames_Basics.spec.js
+++ tests/04_Code_Frames_Basics.spec.js
@@ -37,7 +37,7 @@ test.describe.skip('Alrets', ()=>{
     })
 })
 
-test.describe('Popups,MouseandKeyboard',()=>{
+test.describe('Iframes',()=>{
     test.beforeEach('Excecute1st',async({page})=>{
         await page.goto('https://demo.automationtesting.in/');
         console.log('Navigated to Automation demo site')
@@ -51,12 +51,39 @@ test.describe('Popups,MouseandKeyboard',()=>{
     test('iframes', async({page})=>{
         await page.waitForTimeout(3000);
         const frame= await page.frameLocator('[name="SingleFrame"]')
-        await page.waitForTimeout(3000);
-        frame.locator('//input[@type="text"]').fill('Written under Frames text box');
-        await page.waitForTimeout(3000);
+        // const frame= await page.frameLocator({url:"https://demo.automationtesting.in/SingleFrame.html" })
+        // await page.waitForTimeout(3000);
+        await page.locator('iframe[name="SingleFrame"]').contentFrame().locator('div').filter({ hasText: 'iFrame Demo' }).click();
+        await page.locator('iframe[name="SingleFrame"]').contentFrame().getByRole('textbox').fill('hi');
+        // frame.locator('//input[@type="text"]').fill('Written under Frames text box');
+        // await page.waitForTimeout(3000);
 
     })
+})
 
+test.describe('MouseandKeyboard_Actions',()=>{
+    test.beforeEach('Start_first',async({page})=>{
+        await page.goto('https://demo.automationtesting.in/');
+        console.log('Navigated to Automation demo site')
+        await page.locator('#btn2').click();
+        await page.waitForTimeout(3000);
+        await page.locator("(//a)[contains(text(),'SwitchTo')]").click();
+        await page.waitForTimeout(3000);
+    })
+
+    test('Mouse_actions', async({page})=>{
+        await page.locator('[href="Interactions.html"]').hover();
+        expect(page.getByText('Drag and Drop ')).toBeVisible();
+        await page.locator("//a[text()='Video']").hover();
+        await page.locator('[href="Interactions.html"]').dblclick();
+        await page.getByText('Drag and Drop ').click();
+        await page.locator('[href="Static.html"]').click();
+        await page.waitForTimeout(3000);
+        const source = await page.locator('[src="selenium.png"]');
+        const target = await page.locator('[class="dragged"]');
+ 
+        await source.dragTo(target);
+    })
 
 
 
